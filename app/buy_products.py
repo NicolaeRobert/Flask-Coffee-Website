bf4b2cb36f00bc006f56b764bc8e541a75421c71
NicolaeRobert <rnicolae602@gmail.com>
2025-12-12 15:25:56 +0200

    Added explanations to the code added

--- app/buy_products.py
+++ app/buy_products.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, session, request, redirect, url_for, flash
 from .utils import are_elements_in_cart
 
+#Create the blueprint for the routes that take care of the buying process
 buy_product=Blueprint("buy",__name__)
 
+#The route for buying a dark coffee product
 @buy_product.route("/buy_dark_coffee", methods=["POST"])
 def buy_dark_coffee():
+    #If the user is connected, a product can be bought, and it will be added in the session['cart']
     is_connected="user_id" in session
     if is_connected==True:
         quantity=int(request.form["nr_of_products"])
@@ -12,24 +15,31 @@ def buy_dark_coffee():
 
         return redirect(url_for('main.products'))
     
+    #If the user is not connected we redirect them to the page where they can choose a way to connect or create an account
     flash("You have to be connected in order to buy something","error")
     return redirect(url_for('auth.choose_login_method'))
 
+#The route for changing the number of dark coffee products in the cart
 @buy_product.route("/change_dark_coffee", methods=["POST"])
 def change_dark_coffee():
+    #Get the number that the user wants, change it in the session['cart'] and then redirect back to the page
     quantity=int(request.form["nr_of_products"])
     session["cart"]["dark_coffee"]=quantity
 
     return redirect(url_for('buy.cart'))
 
+#The route to delete the dark coffee products from the cart
 @buy_product.route("/delete_dark_coffee", methods=["POST"])
 def delete_dark_coffee():
+    #Delete them by setting the quantity of that product to 0, then redirect back to the page
     session["cart"]["dark_coffee"]=0
 
     return redirect(url_for('buy.cart'))
 
+#The route to buy a nespreso beens product
 @buy_product.route("/buy_nespreso_beens", methods=["POST"])
 def buy_nespreso_beens():
+    #If the user is connected, a product can be bought, and it will be added in the session['cart']
     is_connected="user_id" in session
     if is_connected==True:
         quantity=int(request.form["nr_of_products"])
@@ -37,24 +47,31 @@ def buy_nespreso_beens():
 
         return redirect(url_for('main.products'))
     
+    #If the user is not connected we redirect them to the page where they can choose a way to connect or create an account
     flash("You have to be connected in order to buy something","error")
     return redirect(url_for('auth.choose_login_method'))
 
+#The route to changing the number of nespreso beens products in the cart
 @buy_product.route("/change_nespreso_beens", methods=["POST"])
 def change_nespreso_beens():
+    #Get the number that the user wants, change it in the session['cart'] and then redirect back to the page
     quantity=int(request.form["nr_of_products"])
     session["cart"]["nespreso_beens"]=quantity
 
     return redirect(url_for('buy.cart'))
 
+#The route for deleting the nespreso beens products from the cart
 @buy_product.route("/delete_nespreso_beens", methods=["POST"])
 def delete_nespreso_beens():
+    #Delete them by setting the quantity of that product to 0, then redirect back to the page
     session["cart"]["nespreso_beens"]=0
 
     return redirect(url_for('buy.cart'))
 
+#The route to buy a brew product
 @buy_product.route("/buy_brew", methods=["POST"])
 def buy_brew():
+    #If the user is connected, a product can be bought, and it will be added in the session['cart']
     is_connected="user_id" in session
     if is_connected==True:
         quantity=int(request.form["nr_of_products"])
@@ -62,24 +79,31 @@ def buy_brew():
 
         return redirect(url_for('main.products'))
     
+    #If the user is not connected we redirect them to the page where they can choose a way to connect or create an account
     flash("You have to be connected in order to buy something","error")
     return redirect(url_for('auth.choose_login_method'))
 
+#The route to change the number of brew products in the cart
 @buy_product.route("/change_brew", methods=["POST"])
 def change_brew():
+    #Get the number that the user wants, change it in the session['cart'] and then redirect back to the page
     quantity=int(request.form["nr_of_products"])
     session["cart"]["brew"]=quantity
 
     return redirect(url_for('buy.cart'))
 
+#The route to delete the brew products from the cart
 @buy_product.route("/delete_brew", methods=["POST"])
 def delete_brew():
+    #Delete them by setting the quantity of that product to 0, then redirect back to the page
     session["cart"]["brew"]=0
 
     return redirect(url_for('buy.cart'))
 
+#The route to buy capsules products
 @buy_product.route("/buy_capsules", methods=["POST"])
 def buy_capsules():
+    #If the user is connected, a product can be bought, and it will be added in the session['cart']
     is_connected="user_id" in session
     if is_connected==True:
         quantity=int(request.form["nr_of_products"])
@@ -87,26 +111,36 @@ def buy_capsules():
 
         return redirect(url_for('main.products'))
     
+    #If the user is not connected we redirect them to the page where they can choose a way to connect or create an account
     flash("You have to be connected in order to buy something","error")
     return redirect(url_for('auth.choose_login_method'))
 
+#The route to change the number of capsules products in the cart
 @buy_product.route("/change_capsules", methods=["POST"])
 def change_capsules():
+    #Get the number that the user wants, change it in the session['cart'] and then redirect back to the page
     quantity=int(request.form["nr_of_products"])
     session["cart"]["capsules"]=quantity
 
     return redirect(url_for('buy.cart'))
 
+#The route to delete the capsules products in the cart
 @buy_product.route("/delete_capsules", methods=["POST"])
 def delete_capsules():
+    #Delete them by setting the quantity of that product to 0, then redirect back to the page
     session["cart"]["capsules"]=0
 
     return redirect(url_for('buy.cart'))
 
+#The route that redirects to the cart page
 @buy_product.route("/cart")
 def cart():
+    #Check if there is a session(it means that the user is connected)
     account_show="user_id" in session
     if(account_show==True):
+        #Get a bool saying if there are elements in the cart
         is_empty=are_elements_in_cart()
+        #Return the cart page with the 2 parameters from above plus the number added in the cart for each product
         return render_template("cart.html", account_show=account_show,is_empty=is_empty,dark_coffee=str(session["cart"]["dark_coffee"]),nespreso_beens=str(session["cart"]["nespreso_beens"]),brew=str(session["cart"]["brew"]),capsules=str(session["cart"]["capsules"]))
+    #Return the same page, but transmit only one parameter, that means that the user is not connected
     return render_template("cart.html", account_show=account_show)
--- app/stripe.py
+++ app/stripe.py
@@ -6,15 +6,23 @@ import stripe
 import os
 from pprint import pprint
 
+#Create the blueprint for the stripe implementation
 stripe_implementation=Blueprint('stripe',__name__)
 
+#Get the stripe secret key that we receive in the sandbox
 stripe.api_key=os.getenv("SECRET_KEY_STRIPE")
 
+#The route to redirect the user to stripe
 @stripe_implementation.route('/redirect_to_stripe')
 def redirect_to_stripe():
+
+    #Here we create the objects bought parameter that we will pass to line_items
     objects_bought=[]
+    #Iterate through the cart
     for key in session["cart"]:
+        #Check to see if the current element was added
         if session['cart'][key]!=0:
+            #Add the object to the parameter
             env_string=key.upper()
             objects_bought.append(
                 {
@@ -23,65 +31,79 @@ def redirect_to_stripe():
                 }
             )
 
-        checkout=stripe.checkout.Session.create(
-            line_items=objects_bought,
-            mode="payment",
-            success_url=url_for('stripe.success', _external=True),
-            cancel_url=url_for('stripe.cancel', _external=True)
-        )
+    #Create the checkout session
+    checkout=stripe.checkout.Session.create(
+        line_items=objects_bought,#The object bought with the quantities
+        mode="payment",#The mode set to payment
+        success_url=url_for('stripe.success', _external=True),#The page where stripe redirects after everything was completed
+        cancel_url=url_for('stripe.cancel', _external=True)#The page where stripe redirect if the user cancels
+    )
 
-        return redirect(checkout.url)
+    #Redirect to the checkout stripe page
+    return redirect(checkout.url)
     
-    return render_template('cancel_page.html')
-
+#The page where stripe redirects after everything was completed
 @stripe_implementation.route('/success')
 def success():
     return render_template('success_page.html')
 
+#The page where stripe redirect if the user cancels
 @stripe_implementation.route('/cancel')
 def cancel():
     return render_template('cancel_page.html')
 
+#The webhook that is being called if a payment failed
 @stripe_implementation.route("/webhook_payment_failed", methods=["POST"])
 def webhook_failed():
-    
+
+    #Delete old wekhook that have more that 7 days
     delete_old_webhooks(False)
 
+    #Get the body od the request along with a parameter from the header
     body=request.data
     stripe_signature=request.headers.get('Stripe-Signature')
 
+    #Get the endpoint secret from .env file(we have received it when we created the webhook)
     endpoint_secret=os.getenv("ENDPOINT_SECRET_FAIL")
 
+    #Here we create the object received
     try:
         event=stripe.Webhook.construct_event(
             body,
             stripe_signature,
             endpoint_secret
         )
+    #This error means that we had a problem with the body(payload)
     except ValueError:
         return 'Invalid payload',400
+    #This error means that we had a problem with the stripe signature, the parameter from the header
     except stripe.error.SignatureVerificationError:
         return 'Invalid signature',400
     
+    #Here we get the connection and the cursor for the database
     conn=get_connection()
     mycursor=get_cursor()
 
+    #See if we already processed the call(stripe sends multiple calls)
     mycursor.execute(
         "SELECT id FROM webhook_fail WHERE id=%s",
         (event['id'],)
     )
-
     id=mycursor.fetchone()
 
+    #If we didn't process the call, do it here
     if id==None and event['type']=="payment_intent.payment_failed":
         
+        #Insert the id and email into the database to consider it processed for the next calls
         mycursor.execute(
             'INSERT INTO webhook_fail (id,email) VALUES (%s,%s)',
             (event['id'],event['receipt_email'])
         )
 
+        #Commit the changes
         conn.commit()
 
+        #Create a message of failure for the user 
         message_for_payment_failure=EmailMessage(
             subject="Nesso - Payment Failed",
             body="""
@@ -93,53 +115,70 @@ def webhook_failed():
             reply_to=[os.getenv("EMAIL")]
         )
 
+        #Send the message
         message_for_payment_failure.send()
 
+    #Close the cursor connection
     mycursor.close()
 
+    #Return 200 OK, because if we got here everything went great
     return "OK",200
 
 @stripe_implementation.route('/webhook_payment_success', methods=["POST"])
 def webhook_succeeded():
 
+    #Delete old wekhook that have more that 7 days 
     delete_old_webhooks(True)
 
+    #Get the body od the request along with a parameter from the header
     body=request.data
     stripe_signature=request.headers.get('Stripe-Signature')
+
+    #Get the endpoint secret from .env file(we have received it when we created the webhook)
     endpoint_secret=os.getenv("ENDPOINT_SECRET_SUCCESS")
 
+    #Here we create the object received
     try:
         event=stripe.Webhook.construct_event(
             body,
             stripe_signature,
             endpoint_secret
         )
+    #This error means that we had a problem with the body(payload)
     except ValueError:
         return 'Invalid payload',400
+    #This error means that we had a problem with the stripe signature, the parameter from the header
     except stripe.error.SignatureVerificationError:
         return 'Invalid signature',400
     
+    #Here we get the connection and the cursor for the database
     conn=get_connection()
     mycursor=get_cursor()
 
+    #See if we already processed the call(stripe sends multiple calls)
     mycursor.execute(
         "SELECT id FROM webhook_success WHERE id=%s",
         (event["id"],)
     )
     id=mycursor.fetchone()
     
+    #If we didn't process the call, do it here
     if id==None and event['type']=='payment_intent.succeeded':
         
+        #Now that the payment was completet we can empty the cart
         for key in session["cart"]:
             session["cart"][key]=0
 
+        #Add the id and the email in the database to consider it processed for next calls
         mycursor.execute(
             "INSERT INTO webhook_success (id,email) VALUES (%s,%s)",
             (event['id'],"roby36474@gmail.com")
         )
 
+        #Commit the changes
         conn.commit()
 
+        #Create the cmfirmation email
         comfirmation_message=EmailMessage(
             subject='Nesso - Payment Comfirmation',
             body='The payment was successful. Thank you for choosing out products!',
@@ -148,9 +187,11 @@ def webhook_succeeded():
             reply_to=[os.getenv('EMAIL')]
         )
 
+        #Send the email
         comfirmation_message.send()
 
-    
+    #Close the cursor
     mycursor.close()
 
+    #Return 200 OK, because if we got here everything went great
     return 'OK',200
